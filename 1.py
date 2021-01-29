b9e1f03e50f664c2aa4d578e9b00436c06cb36c5
rui999-lab <75787019+rui999-lab@users.noreply.github.com>
2021-01-29 10:13:00 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+import random
+
+"""list=[]
+for i in  range(12):
+    num=(random.randint(0,6))
+    list.append(num)
+
+    print(num,end="")"""
+
+liste =[i for i in range(15)]
+print(liste)
---
+++ import scrip.py
@@ -0,0 +1,8 @@
+import pandas
+
+
+print(pandas.__version__)
+de= pandas.read_csv("C:heart.tex",sep='\t',header = 0)
+print( type(df))
+print(df.shape)
+print(df.heap(100))
